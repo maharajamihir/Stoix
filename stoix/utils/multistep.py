fix: subtract last value during return calculation

stoix/utils/multistep.py
@@ -94,6 +94,7 @@ def batch_truncated_generalized_advantage_estimation(
     )
 
     target_values = values[:-1] + advantage_t
+    target_values -= values[-1, :].reshape(1,-1)
 
     if not time_major:
         advantage_t = jnp.transpose(advantage_t, (1, 0))
@@ -105,7 +106,6 @@ def batch_truncated_generalized_advantage_estimation(
     if stop_target_gradients:
         advantage_t = jax.lax.stop_gradient(advantage_t)
         target_values = jax.lax.stop_gradient(target_values)
-
     return advantage_t, target_values
 
 